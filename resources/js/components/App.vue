62d561635e755c48d70da5b0099c711a5c0547e9
Grldk <gerald@neg-itsolutions.nl>
2023-06-30 16:56:01 +0200

    fix app.vue

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -32,7 +32,7 @@ export default {
 
           <h1 class="h4 mb-0 ml-2">
             <strong>Laravel</strong> Horizon{{
-              config("app.name") ? " - ".config("app.name") : ""
+              appName
             }}
           </h1>
         </router-link>
@@ -193,19 +193,17 @@ export default {
         </div>
 
         <div class="col-10">
-          @if (! $assetsAreCurrent)
-          <div class="alert alert-warning">
+          <div v-if="!assetsAreCurrent" class="alert alert-warning">
             The published Horizon assets are not up-to-date with the installed
             version. To update, run:<br /><code
               >php artisan horizon:publish</code
             >
           </div>
-          @endif @if ($isDownForMaintenance)
-          <div class="alert alert-warning">
+
+          <div v-if="isDownForMaintenance" class="alert alert-warning">
             This application is in "maintenance mode". Queued jobs may not be
             processed unless your worker is using the "force" flag.
           </div>
-          @endif
 
           <router-view></router-view>
         </div>
