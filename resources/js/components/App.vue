1681543a9e05cfa70dd10fde85a905c89535200f
Grldk <gerald@neg-itsolutions.nl>
2024-05-09 12:57:28 +0200

    - remove assetsAreCurrent check - npm run build

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,6 +1,6 @@
 <script type="text/ecmascript-6">
 export default {
-  props: ["assetsAreCurrent", "isDownForMaintenance", "appName"],
+  props: ["isDownForMaintenance", "appName"],
   data() {
     return {
       alert: this.$root.alert,
@@ -193,13 +193,6 @@ export default {
         </div>
 
         <div class="col-10">
-          <div v-if="!assetsAreCurrent" class="alert alert-warning">
-            The published Horizon assets are not up-to-date with the installed
-            version. To update, run:<br /><code
-              >php artisan horizon:publish</code
-            >
-          </div>
-
           <div v-if="isDownForMaintenance" class="alert alert-warning">
             This application is in "maintenance mode". Queued jobs may not be
             processed unless your worker is using the "force" flag.
